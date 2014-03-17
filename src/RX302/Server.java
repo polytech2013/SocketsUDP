bbc60de33c2d07a65bd03c08301fab32d00da83f
p1308211 <p1308211@EPULWKS60.univ-lyon1.fr>
2014-03-17 09:57:36 +0100

    Cleanup

--- src/RX302/Server.java
+++ src/RX302/Server.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
